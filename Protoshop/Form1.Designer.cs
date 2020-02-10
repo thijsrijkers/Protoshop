ed379b15af0b80f98977229541bc37a1608b3e29
thijsrijkers <gamearea20@gmail.com>
2020-02-10 20:05:47 +0100

    Protoshop Icon added

--- Protoshop/Form1.Designer.cs
+++ Protoshop/Form1.Designer.cs
@@ -287,6 +287,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
